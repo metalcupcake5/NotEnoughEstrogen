d225edb73ac4835269713b9b4fd740ea4148ec4f
metalcupcake5 <imcoolcom5@gmail.com>
2022-01-22 16:19:50 -0800

    refactor: change logger prefix

--- src/main/java/io/github/metalcupcake5/JustEnoughUpdates/JustEnoughUpdates.java
+++ src/main/java/io/github/metalcupcake5/JustEnoughUpdates/JustEnoughUpdates.java
@@ -18,7 +18,7 @@ public class JustEnoughUpdates implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("test");
+	public static final Logger LOGGER = LogManager.getLogger("justenoughupdates");
 
 	@Override
 	public void onInitialize() {
