724fd38f62944be690ef2ea17c70b1e0added238
metalcupcake5 <imcoolcom5@gmail.com>
2022-01-22 17:43:00 -0800

    feat: use a mod id variable

--- src/main/java/io/github/metalcupcake5/JustEnoughUpdates/JustEnoughUpdates.java
+++ src/main/java/io/github/metalcupcake5/JustEnoughUpdates/JustEnoughUpdates.java
@@ -15,10 +15,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class JustEnoughUpdates implements ModInitializer {
+	public static final String MOD_ID = "justenoughupdates";
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LogManager.getLogger("justenoughupdates");
+	public static final Logger LOGGER = LogManager.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
