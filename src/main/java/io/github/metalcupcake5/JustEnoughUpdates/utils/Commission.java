f1aef0b0a87e554a6bb4b96579eb2a84137ec012
metalcupcake5 <imcoolcom5@gmail.com>
2022-02-20 17:32:58 -0800

    feat: color commission tracker based on progress

--- src/main/java/io/github/metalcupcake5/JustEnoughUpdates/utils/Commission.java
+++ src/main/java/io/github/metalcupcake5/JustEnoughUpdates/utils/Commission.java
@@ -1,5 +1,7 @@
 package io.github.metalcupcake5.JustEnoughUpdates.utils;
 
+import net.minecraft.util.Formatting;
+
 public class Commission {
     private final String name;
     private final double percent;
@@ -10,11 +12,12 @@ public class Commission {
         this.name = name;
         if(percent.equals("DONE")){
             this.percent = 1.0;
-            this.progress = "Done!";
+            this.progress = Formatting.GREEN + "Done!";
         } else {
             this.percent = Double.parseDouble(percent.substring(0, percent.length() - 1)) / 100;
+            String prefix = String.valueOf(this.percent >= 0.75 ? Formatting.GREEN : this.percent >= 0.5 ? Formatting.YELLOW : this.percent >= 0.25 ? Formatting.GOLD : Formatting.RED);
             int total = Commissions.getCommission(name, location).total;
-            this.progress = (int) (this.percent * total) + "/" + total;
+            this.progress = prefix + (int) (this.percent * total) + "/" + total;
         }
         this.location = location;
     }
