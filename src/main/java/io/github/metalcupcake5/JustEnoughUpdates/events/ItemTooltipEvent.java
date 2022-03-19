9401fa46b22367d06f59a12c2a0b53591c50cbbe
metalcupcake5 <imcoolcom5@gmail.com>
2022-03-19 01:23:35 -0700

    refactor: use variables for item info

--- src/main/java/io/github/metalcupcake5/JustEnoughUpdates/events/ItemTooltipEvent.java
+++ src/main/java/io/github/metalcupcake5/JustEnoughUpdates/events/ItemTooltipEvent.java
@@ -25,21 +25,21 @@ public class ItemTooltipEvent {
         if(ConfigManager.showSkyblockIds) {
             String id = attributes.getString("id");
             if (id.length() > 0) {
-                lines.add(Text.of(Formatting.DARK_GRAY + "skyblock id: " + attributes.getString("id")));
+                lines.add(Text.of(Formatting.DARK_GRAY + "skyblock id: " + id));
             }
         }
         if(ConfigManager.showItemInfo) {
             String timestamp = attributes.getString("timestamp");
             if (timestamp.length() > 0) {
-                lines.add(Text.of(Formatting.DARK_GRAY + "timestamp: " + attributes.getString("timestamp")));
+                lines.add(Text.of(Formatting.DARK_GRAY + "timestamp: " + timestamp));
             }
             String origin = attributes.getString("originTag");
             if (origin.length() > 0) {
-                lines.add(Text.of(Formatting.DARK_GRAY + "origin: " + attributes.getString("originTag")));
+                lines.add(Text.of(Formatting.DARK_GRAY + "origin: " + origin));
             }
             String uuid = attributes.getString("uuid");
             if (uuid.length() > 0) {
-                lines.add(Text.of(Formatting.DARK_GRAY + "uuid: " + attributes.getString("uuid")));
+                lines.add(Text.of(Formatting.DARK_GRAY + "uuid: " + uuid));
             }
         }
     }
